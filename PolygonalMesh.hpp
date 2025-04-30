f3727cacbd0facd7273408eda9fc95b61ec8c5ad
BeatriceRuggeri <s298841@studenti.polito.it>
2025-04-30 23:47:41 +0200

    Add files via upload

---
+++ PolygonalMesh.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include "Eigen/Eigen"
+#include <vector>
+
+using namespace std;
+using namespace Eigen;
+
+
+namespace PolygonalLibrary {
+
+struct PolygonalMesh
+{
+    unsigned int NumCell0Ds = 0; 
+    std::vector<unsigned int> Cell0DsId = {};
+    Eigen::MatrixXd Cell0DsCoordinates = {};
+    std::map<unsigned int, list<unsigned int>> MarkerCell0Ds = {}; 
+
+    unsigned int NumCell1Ds = 0; 
+    std::vector<unsigned int> Cell1DsId = {}; 
+    Eigen::MatrixXi Cell1DsExtrema = {};
+    std::map<unsigned int, list<unsigned int>> MarkerCell1Ds = {}; 
+    
+    unsigned int NumCell2Ds = 0;
+    std::vector<unsigned int> Cell2DsId = {}; 
+    map<unsigned int, list<unsigned int>> NumVert2Ds = {};
+    std::vector<vector<unsigned int>> Cell2DsVertices = {}; 
+    map<unsigned int, list<unsigned int>> NumEdges2Ds= {}; 
+    std::vector<vector<unsigned int>> Cell2DsEdges = {}; 
+    
+};
+
+}
---
+++ Utils.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include "PolygonalMesh.hpp"
+
+using namespace std;
+
+namespace calcolo{
+	double EdgeLength(const double& x1, const double& y1,const double& x2, const double& y2);
+}
+
+namespace PolygonalLibrary
+{
+
+bool ImportMesh(PolygonalMesh& mesh);
+
+bool ImportCell0Ds(PolygonalMesh& mesh);
+
+
+bool ImportCell1Ds(PolygonalMesh& mesh);
+
+bool ImportCell2Ds(PolygonalMesh& mesh);
+
+bool CheckEdges(const PolygonalMesh& mesh, const double& eps);
+
+//bool CheckAreas(const PolygonalMesh& mesh, const double& eps);
+
+
+}
